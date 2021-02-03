9bb6e8adcfca0f9f557e707de604ef417f119d4d
unknown <nilakone@verisette.com>
2021-02-03 16:55:08 +0700

    htaccess proaddr

---
+++ User/testnotification.php
@@ -0,0 +1,59 @@
+<?php
+  $title = "ໜ້າຫຼັກ";
+  $path = "../";
+  $links = "";
+  $session_path = "../";
+  include ("../header-footer/header.php");
+?>
+
+<div class="row">
+<div class="col-md-2">
+<ul class="list-group">
+  <li class="list-group-item active"><a href="http://www.nnt.gov.la/">Home</a></li>
+</ul>
+</div>
+
+<div class="col-md-3">
+<ul class="list-group">
+  <li class="list-group-item active"><a href="http://www.nnt.gov.la/about/">The Park</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/landscape">Landscape</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/biodiversity">Biodiversity</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/culture">People & Culture</a></li>
+    <li class="list-group-item"><a href="http://www.nnt.gov.la/conservation">Conservation</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/partners">Partners</a></li>
+</ul>
+</div>
+
+<div class="col-md-3">
+<ul class="list-group">
+  <li class="list-group-item active"><a href="http://www.nnt.gov.la/visit">Visit</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/see/">See</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/do/">Do</a></li>
+  <li class="list-group-item" style="margin-left:35px"><a href="http://www.nnt.gov.la/do/lanhai/">1/2D FISHING & PICNIC AT LAN HAI</a></li>
+    <li class="list-group-item" style="margin-left:35px"><a href="http://www.nnt.gov.la/do/nyim1day">1D NYIM WATERFALL & BEACH PICNIC</a></li>
+      <li class="list-group-item" style="margin-left:35px"><a href="http://www.nnt.gov.la/do/namxot2day/">2D Nam Xot Beach Camping & Swamp Cypress</a></li>
+        <li class="list-group-item" style="margin-left:35px"><a href="http://www.nnt.gov.la/do/namchae/">2-3D Nam Chae (Douc) Trek</a></li>
+          <li class="list-group-item" style="margin-left:35px"><a href="http://www.nnt.gov.la/do/nakaisafari/">2-3D Night Safari & Swamp Cypress</a></li>
+            <li class="list-group-item"><a href="http://www.nnt.gov.la/sleep/">Sleep</a></li>
+  <li class="list-group-item"><a href="http://www.nnt.gov.la/getting-here/">Getting Here</a></li>
+    <li class="list-group-item"><a href="http://www.nnt.gov.la/faqs/">Things to know</a></li>
+</ul>
+</div>
+
+<div class="col-md-2">
+<ul class="list-group">
+  <li class="list-group-item active"><a href="http://www.nnt.gov.la/news">News</a></li>
+</ul>
+</div>
+
+<div class="col-md-2">
+<ul class="list-group">
+  <li class="list-group-item active"><a href="http://www.nnt.gov.la/contact/">Contact</a></li>
+</ul>
+</div>
+</div>
+
+
+<?php
+ include ("../header-footer/footer.php");
+?>
